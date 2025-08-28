2d6f6aa8cfda2194127600c5a61603e2b7a69b0d
Juan Manuel <juan.ferreira.m@estudiantes.utec.edu.uy>
2025-08-28 19:30:14 -0300

    Se agrega un delay antes de apagar los leds ademas de mejorar los commentarios. SIGUE FALTANDO PRUEBA FISICA

--- Evaluacion_Continua/3_Extremos_hacia_Centro/3_Extremos_hacia_Centro/main.asm
+++ Evaluacion_Continua/3_Extremos_hacia_Centro/3_Extremos_hacia_Centro/main.asm
@@ -12,18 +12,20 @@
 .org    0x0000
 rjmp    DECLARAR_VARIABLES
 
-;Se declararan los pines en el R16 y el Timer el R17
+; Se declararan los pines en el R16 y el Timer el R17
 DECLARAR_VARIABLES:
        
+	   ; Stack Pointer
         ldi     r16, high(RAMEND)
         out     SPH, r16
         ldi     r16, low(RAMEND)
         out     SPL, r16
 
-  
+    ; Declaracion de pines del 0 al 8 del Arduino
         ldi     r16, (1<<DDD0) | (1<<DDD1) | (1<<DDD2) | (1<<DDD3) | (1<<DDD4) | (1<<DDD5) | (1<<DDD6) | (1<<DDD7)
         out     DDRD, r16
-		
+	
+	; Declaracion del Timer
 		LDI R17,0
         STS TCCR1A,R17
         LDI R17,(1<<CS12)
@@ -39,33 +41,39 @@ DECLARAR_VARIABLES:
         SEI
 		rjmp    MAIN
 
-; Main principal del cdoigo
+; Main principal del codigo con funcionamiento del mismo
 
 MAIN:
 		ldi r16, (1<<PORTD0) |(1<<PORTD7)
         out PORTD, r16          
 		ldi r16, (1<<PORTD1) |(1<<PORTD6)
         out PORTD, r16
+		call DELAY
 		call APAGARLEDS
 		call DELAY
 		ldi r16, (1<<PORTD2) |(1<<PORTD5)
         out PORTD, r16          
+		call DELAY
 		call APAGARLEDS
 		call DELAY
 		ldi r16, (1<<PORTD3) |(1<<PORTD4)
         out PORTD, r16
+		call DELAY
 		call APAGARLEDS
 		call DELAY
 		ldi r16, (1<<PORTD2) |(1<<PORTD5)
         out PORTD, r16          
+		call DELAY
 		call APAGARLEDS
 		call DELAY
 		ldi r16, (1<<PORTD1) |(1<<PORTD6)
         out PORTD, r16
+		call DELAY
 		call APAGARLEDS
 		call DELAY
 		ldi r16, (1<<PORTD0) |(1<<PORTD7)
         out PORTD, r16
+		call DELAY
 		call APAGARLEDS
 		call DELAY
 		rjmp MAIN
@@ -76,7 +84,7 @@ APAGARLEDS:
             ldi     r16, 0        
 			out     PORTD, r16
 
-;Funcion para el Delay
+; Funcion para el Delay
 DELAY:
         SBIS TIFR1,TOV1
         RJMP DELAY
@@ -86,5 +94,3 @@ DELAY:
         LDI R16,LOW(3036)
         STS TCNT1L,R16
         RET
-
-
