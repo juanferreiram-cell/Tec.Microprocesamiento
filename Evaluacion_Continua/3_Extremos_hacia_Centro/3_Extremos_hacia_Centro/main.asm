af8dc4d18f7b662d5528a65678a603be3c6f27c6
Juan Manuel <juan.ferreira.m@estudiantes.utec.edu.uy>
2025-08-28 18:27:12 -0300

    Codigo final para el apartado de extremos al centro FALTA PRUEBA EN FISICO

--- Evaluacion_Continua/3_Extremos_hacia_Centro/3_Extremos_hacia_Centro/main.asm
+++ Evaluacion_Continua/3_Extremos_hacia_Centro/3_Extremos_hacia_Centro/main.asm
@@ -1,58 +1,90 @@
 ;
 ; 3_Extremos_hacia_Centro.asm
 ;
-; Created: 28/8/2025 14:49:51
-; Author : juanm
+; Created: 28/8/2025
+; Author : Lucas Elizalde, Juan Manuel Ferreira, Felipe Morrudo
 ;
 
 
-; Replace with your application code
+.include "m328pdef.inc"
 
-        .include "m328pdef.inc"
+.cseg
+.org    0x0000
+rjmp    DECLARAR_VARIABLES
 
-        .cseg
-        .org    0x0000
-        rjmp    INICIO
-
+;Se declararan los pines en el R16 y el Timer el R17
 DECLARAR_VARIABLES:
-        ; --- Stack ---
+       
         ldi     r16, high(RAMEND)
         out     SPH, r16
         ldi     r16, low(RAMEND)
         out     SPL, r16
 
-        ; --- LED en PB5 como salida ---
+  
         ldi     r16, (1<<DDD0) | (1<<DDD1) | (1<<DDD2) | (1<<DDD3) | (1<<DDD4) | (1<<DDD5) | (1<<DDD6) | (1<<DDD7)
         out     DDRD, r16
+		
+		LDI R17,0
+        STS TCCR1A,R17
+        LDI R17,(1<<CS12)
+        STS TCCR1B,R17
+        LDI R16,0
+        STS TCCR1C,R17
+        LDI R17,0
+        STS TIMSK1,R17
+        LDI R17,HIGH(3036)
+        STS TCNT1H,R17
+        LDI R17,LOW(3036)
+        STS TCNT1L,R17
+        SEI
+		rjmp    MAIN
+
+; Main principal del cdoigo
 
 MAIN:
 		ldi r16, (1<<PORTD0) |(1<<PORTD7)
-        out PORTD, r16          ; Escribe en el registro de salida
-		rjmp APAGARLEDS
+        out PORTD, r16          
 		ldi r16, (1<<PORTD1) |(1<<PORTD6)
         out PORTD, r16
-		rjmp APAGARLEDS
+		call APAGARLEDS
+		call DELAY
 		ldi r16, (1<<PORTD2) |(1<<PORTD5)
-        out PORTD, r16          ; Escribe en el registro de salida
-		rjmp APAGARLEDS
+        out PORTD, r16          
+		call APAGARLEDS
+		call DELAY
 		ldi r16, (1<<PORTD3) |(1<<PORTD4)
         out PORTD, r16
-		rjmp APAGARLEDS
+		call APAGARLEDS
+		call DELAY
 		ldi r16, (1<<PORTD2) |(1<<PORTD5)
-        out PORTD, r16          ; Escribe en el registro de salida
-		rjmp APAGARLEDS
+        out PORTD, r16          
+		call APAGARLEDS
+		call DELAY
 		ldi r16, (1<<PORTD1) |(1<<PORTD6)
         out PORTD, r16
-		rjmp APAGARLEDS
+		call APAGARLEDS
+		call DELAY
 		ldi r16, (1<<PORTD0) |(1<<PORTD7)
-        out PORTD, r16          ; Escribe en el registro de salida
-		rjmp APAGARLEDS
+        out PORTD, r16
+		call APAGARLEDS
+		call DELAY
 		rjmp MAIN
 
+
+; Funcion que apaga los LEDS
 APAGARLEDS:
-            ldi     r16, 0            ; Todos los pines de PORTD en bajo
+            ldi     r16, 0        
 			out     PORTD, r16
 
-
+;Funcion para el Delay
+DELAY:
+        SBIS TIFR1,TOV1
+        RJMP DELAY
+        SBI TIFR1,TOV1
+        LDI R16,HIGH(3036)
+        STS TCNT1H,R16
+        LDI R16,LOW(3036)
+        STS TCNT1L,R16
+        RET
 
 
